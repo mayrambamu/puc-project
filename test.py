7532ce4a1ceea61b1b9511e107a9b85c2dd58d58
mayrambamu <mbamu.datanalist@gmail.com>
2022-11-29 15:08:21 -0500

    'add'

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import pytest
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 from fastapi.testclient import TestClient
-from main import app
+from main import app, scaler, model
 
 client = TestClient(app)
 
 def test_home():
     response = client.get('/')
     assert response.json() == {'Hello': 'World'}
+
+
+def test_models():
+    assert isinstance(scaler, StandardScaler)
+    assert isinstance(model, RandomForestClassifier)
